2740e3dcce3ed8308f1e945563fbf6955772def7
Christian Lopes <christian.lima@ntendencia.com.br>
2023-11-27 20:37:36 -0300

    FEAT: Adicionando campo para melhorar segurança dos tokens.

--- src/main/java/com/example/watchrecommendation/module/user/entity/User.java
+++ src/main/java/com/example/watchrecommendation/module/user/entity/User.java
@@ -25,6 +25,10 @@ public class User implements UserDetails {
     @Column(unique = true)
     private String phone;
 
+    private String tokenAssignature;
+
+    private String birthDate;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return new ArrayList<>();
